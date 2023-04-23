06f42e37020646ead2a7669dedc5342173b29911
tkulagina <tkulagina1974@gmail.com>
2023-04-23 18:42:36 +0300

    add simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,4 +3,7 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-console.log('hello')
+//console.log('hello')
+
+//import SimpleLightbox from "simplelightbox";
+//import "simplelightbox/dist/simple-lightbox.min.css";
