ee8d9b3f0eaccec87a88331a81589faead795a74
tkulagina <tkulagina1974@gmail.com>
2023-04-24 13:33:43 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+console.log(throttle);
+console.log(Player);
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onTimeupdate = function ({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+const timeStart = localStorage.getItem('videoplayer-current-time');
+
+if (timeStart) {
+  player
+    .setCurrentTime(timeStart)
+    .then(function (seconds) {
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+
+        default:
+          break;
+      }
+    });
+}
