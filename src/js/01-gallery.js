49f1af2521e7ecee4655f6036ee63cbb610adab3
tkulagina <tkulagina1974@gmail.com>
2023-04-24 12:03:14 +0300

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,6 +5,25 @@ import { galleryItems } from './gallery-items';
 console.log(galleryItems);
 //console.log('hello')
 
-//import SimpleLightbox from "simplelightbox" ;
+import SimpleLightbox from "simplelightbox";
 
-//import "simplelightbox/dist/simple-lightbox.min.css";
+console.log(SimpleLightbox);
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+const listRef = document.querySelector('.gallery');
+const markup = galleryItems.map(({preview, original, description}) => `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`).join('');
+
+listRef.insertAdjacentHTML('beforeend', markup)
+
+
+new SimpleLightbox('.gallery__item a', {
+    captions: true,
+    captionType: 'attr', 
+    captionsData: `alt`,
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
