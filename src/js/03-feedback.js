fac0652a704262eee0e198da5f30fae585a9f5d8
tkulagina <tkulagina1974@gmail.com>
2023-04-24 13:46:51 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
+console.log(throttle);
 
+const feedbackForm = document.querySelector(".feedback-form");
+
+const savedFormDataJSON = localStorage.getItem("feedback-form-state");
+const savedFormData = JSON.parse(savedFormDataJSON);
+
+if (savedFormData !== null) {
+    feedbackForm["email"].value = savedFormData.email;
+    feedbackForm["message"].value = savedFormData.message;
+};
+
+feedbackForm.addEventListener("input", throttle(event => {
+
+    const formData = { email: `${feedbackForm["email"].value}`, message: `${feedbackForm["message"].value}` };
+    const formDataJSON = JSON.stringify(formData);
+
+    localStorage.setItem("feedback-form-state", formDataJSON);
+
+}, 500));
+
+feedbackForm.addEventListener("submit", event => {
+    event.preventDefault();
+    
+    const formData = { email: `${feedbackForm["email"].value}`, message: `${feedbackForm["message"].value}` };
+    console.log(formData);
+
+    localStorage.removeItem("feedback-form-state");
+    feedbackForm["email"].value = "";
+    feedbackForm["message"].value = "";
+
+});
